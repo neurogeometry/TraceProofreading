e983d2a2ca8afcd5b82faea26b21a7a75b0ef1ff
mousavikahaki <mousavikahaki@gmail.com>
2021-02-16 12:15:09 -0500

    Modify evaluation

--- common_functions/Evaluation/Rohan_Plots.m
+++
@@ -1,9 +0,0 @@
-function [] = Rohan_Plots(IndexStr)
-
-trace_hist = hist(IndexStr.Dag_trace_full,0.25:0.5:4.75);
-TP_hist = hist(IndexStr.Dag_TP_full,0.25:0.5:9.75);
-BP_hist = hist(IndexStr.Dag_BP_full,0.25:0.5:9.75);
-
-figure,plot(2*trace_hist/length(IndexStr.Dag_trace_full),'.-')
-figure,plot(2*TP_hist/length(IndexStr.Dag_TP_full),'.-')
-figure,plot(2*BP_hist/length(IndexStr.Dag_BP_full),'.-')
